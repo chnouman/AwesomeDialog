a03dcd784ae0648a2fc6bcf1fcd7ebdd31fa98fc
MuhammadNouman <chnouman200@gmail.com>
2020-08-15 15:45:24 +0500

    Minimum SDK set to 15

--- awesomeDialog/src/main/java/com/example/awesomedialog/AwesomeDialog.kt
+++ awesomeDialog/src/main/java/com/example/awesomedialog/AwesomeDialog.kt
@@ -4,29 +4,12 @@ import android.app.Activity
 import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
-import com.example.awesomedialog.AwesomeDialog.Companion.icon
 import kotlinx.android.synthetic.main.awesome_dilaog.*
 
 class AwesomeDialog() {
 
     companion object {
-
-
-        const val LONG_DURATION = 5000 // 5 seconds
-        const val SHORT_DURATION = 2000 // 2 seconds
-        const val TOAST_SUCCESS = "SUCCESS"
-        const val TOAST_ERROR = "FAILED"
-        const val TOAST_WARNING = "WARNING"
-        const val TOAST_INFO = "INFO"
-        const val TOAST_DELETE = "DELETE"
-        const val TOAST_NO_INTERNET = "NO INTERNET"
-
-
-        const val GRAVITY_TOP = 48
-        const val GRAVITY_CENTER = 20
-        const val GRAVITY_BOTTOM = 80
 
         private lateinit var layoutInflater: LayoutInflater
 
